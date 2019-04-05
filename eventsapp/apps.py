5fb707d5a02ed30997bb7d0ebda8ffaa0dd0500d
gosport-geekbrains <49106313+gosport-geekbrains@users.noreply.github.com>
2019-04-05 18:51:05 +0300

    Revert "Feature/initial static file"

--- eventsapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventsappConfig(AppConfig):
-    name = 'eventsapp'
--- venuesapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VenuesappConfig(AppConfig):
-    name = 'venuesapp'
