58f2c49d633840fd9eefc7219a4cc41be68945a8
Khetag Tebiev <kurtatag@gmail.com>
2019-04-11 00:43:59 +0300

    Add template for flash messages

--- gosport_project/settings.py
+++ gosport_project/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "gosport_project/static"),
 ]
+
+
+# Messages
+from django.contrib.messages import constants
+MESSAGE_TAGS = {
+    constants.ERROR: 'danger'
+}
