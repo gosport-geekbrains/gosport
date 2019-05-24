341bee67978fb55df333a22e9def83d17a5c69b5
texikator <wiktot@mail.ru>
2019-05-24 13:55:44 +0300

    tmp

---
+++ venuesapp/management/commands/thumbs_create.py
@@ -0,0 +1,21 @@
+﻿from django.core.management.base import BaseCommand, CommandError
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+
+
+#from venuesapp.models import CategoryClear
+from venuesapp.models import Photo
+
+
+
+
+class Command(BaseCommand):
+    help = 'create thumbs'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--clear',
+                            action='remove_all',
+                            help='clear all thumbs')
+
+    def handle(self, *args, **options):
+        pass
